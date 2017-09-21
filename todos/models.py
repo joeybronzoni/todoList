5505282d3ed96d66f4c58a792f9261481bc8fd4e
josephskrzysowski <josephskrzysowski@gmail.com>
2017-09-20 22:27:04 -0400

    setting up

--- todos/models.py
+++ todos/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from datetime import datetime
+
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    
+  
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from django.shortcuts import render
+#bring in the HttpResponse
 from django.http import HttpResponse
 
 # Create your views here.
+
+# when index is loaded give us the request object
 def index(request):
     return HttpResponse('Hello Wolrd')
